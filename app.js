ad87bcacfb1d3470a8f6214af05bc6000f68aaea
Emmanuel Isaac <emmanuel.i157@yahoo.com.ar>
2025-07-26 17:59:30 -0300

    add: agregarAmigos & mostrarLista

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo) {
+        listaDeAmigos.push(amigo);
+        document.getElementById("amigo").value = "";
+        mostrarListaDeAmigos();
+    } else {
+        alert("Por favor, ingresa un nombre.");
+    }
+    console.log("Lista de amigos inicial:", listaDeAmigos);
+}
+
+function mostrarListaDeAmigos() {
+    let lista = document.getElementById("listaAmigos");
+
+    // Limpiar el contenido previo
+    lista.innerHTML = "";
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+        lista.appendChild(li);
+    }
+}
